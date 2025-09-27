a46c964d155966c38fd40f2c94e99316771afaf8
vytautasmedia <uvytas987@gmail.com>
2025-09-27 16:19:14 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-module.exports={content:["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}"],theme:{extend:{colors:{brand:{DEFAULT:"#1e90ff",dark:"#1769b1",light:"#eaf4ff"}}}},plugins:[]};
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        // GST paletė
+        brand: {
+          900: "#624173", // tamsus violetas (R98 G65 B115)
+          700: "#9C85AD", // (R156 G133 B173)
+          600: "#AD93BF", // (R173 G147 B191)
+          300: "#CAB2DB", // (R202 G178 B219)
+          DEFAULT: "#624173",
+          light: "#EDE6F3"
+        }
+      },
+      borderRadius: { "2xl": "1rem" }
+    },
+  },
+  plugins: [],
+};
